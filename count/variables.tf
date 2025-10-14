4cd27ce9ef8da2595d7876c0cce7babaafb98a7e
Anil1931 <hanshpappa31@gmail.com>
2025-10-15 01:30:31 +0530

    terraform

---
+++ count/variables.tf
@@ -0,0 +1,11 @@
+variable "instances" {
+    default = [ "mongodb", "redis" , "mysql" , "rabbitmq" ]
+}
+
+variable "zone_id" {
+  default = "Z0061673127ECQ2F5XQ3Q"
+}
+
+variable "domain_name" {
+    default = "hansh123.online"
+}
